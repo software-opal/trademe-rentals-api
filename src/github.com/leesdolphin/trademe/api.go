a33c2e3bdb0adf874de13fe7a2315dd68ba021de
Lee Symes <leesdolphin@gmail.com>
2015-06-11 00:04:13 +1200

    Got my first listing - In other news I really hate searching for properties.

--- src/github.com/leesdolphin/trademe/api.go
+++ src/github.com/leesdolphin/trademe/api.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"regexp"
@@ -19,18 +21,18 @@ func (p badSearchPage) Error() string {
 }
 
 type propertyData struct {
-	listingId          string
-	uri                string
-	title, description string
-	otherData          map[string]string
-	price              float64
-	images             []string
-	locationData       propertyLocationData
+	ListingId          string
+	Uri                string
+	Title, Description string
+	OtherData          map[string]string
+	Price              float64
+	Images             []string
+	LocationData       propertyLocationData
 }
 
 type propertyLocationData struct {
-	lat, long      float64
-	street, suburb string
+	Lat, Lng       float64
+	Street, Suburb string
 }
 
 var propertyURLRegex = regexp.MustCompile("http[s]?:\\/\\/www\\.trademe\\.co\\.nz\\/property\\/residential-property[a-z\\-]+?/auction-\\d+.htm")
@@ -77,8 +79,7 @@ func loadPropertyFromSeedURLs(seedUrls []string, propertyUrls chan *url.URL) err
 					continue
 				}
 				searchPageUrls[searchUrl.String()] = true
-				fmt.Println(searchUrl)
-				loadPropertyFromURLList(searchUrl, searchPageChan, searchResultsChan)
+				loadPropertiesFromURL(searchUrl, searchPageChan, searchResultsChan)
 			default: // Run out of pages. Die.
 				// No more to add to results.
 				close(searchResultsChan)
@@ -114,12 +115,34 @@ func main() {
 		return
 	}
 
-	for uri := range propertyURLs {
-		fmt.Println(loadPropertyDataFrom(uri))
+	propertyDataChan := multiplex(10, func(thisChan chan *propertyData) {
+		for uri := range propertyURLs {
+			data := loadPropertyDataFrom(uri)
+			if data != nil {
+				thisChan <- data
+			}
+		}
+		close(thisChan)
+	})
+
+	listingIdMapping := make(map[string]*propertyData)
+	for data := range propertyDataChan {
+		listingIdMapping[data.ListingId] = data
+	}
+	listingJson, err := json.Marshal(listingIdMapping)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = ioutil.WriteFile("testing.json", listingJson, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 }
 
-func loadPropertyFromURLList(baseURL *url.URL, searchPageChan, searchResultsChan chan *url.URL) error {
+func loadPropertiesFromURL(baseURL *url.URL, searchPageChan, searchResultsChan chan *url.URL) error {
+	fmt.Println("Starting " + baseURL.String()[60:])
 	resp, err := http.Get(baseURL.String())
 	if err != nil {
 		return err
@@ -127,13 +150,19 @@ func loadPropertyFromURLList(baseURL *url.URL, searchPageChan, searchResultsChan
 	defer resp.Body.Close()
 	z := html.NewTokenizer(resp.Body)
 	for {
-		tt := z.Next()
-		if tt == html.ErrorToken {
+		switch z.Next() {
+		case html.EndTagToken:
+			if getTagName(z) != "html" {
+				break
+			}
+			fallthrough
+		case html.ErrorToken:
 			// Probably the end of the file ...
-			fmt.Println("Error: ", z.Err())
-			break
-		}
-		if tt == html.StartTagToken {
+			if err = z.Err(); err != nil {
+				fmt.Println("PFURLError: ", err)
+			}
+			return nil
+		case html.StartTagToken:
 			tagName := getTagName(z)
 			switch tagName {
 			case "a":
@@ -160,8 +189,6 @@ func loadPropertyFromURLList(baseURL *url.URL, searchPageChan, searchResultsChan
 			}
 		}
 	}
-	fmt.Println("Done loading search results")
-	return nil
 }
 
 func getTagName(z *html.Tokenizer) string {
@@ -267,57 +294,70 @@ func readTextFromTagWithAttr(z *html.Tokenizer, tagName, attrName, attrValue str
 	return text, nil
 }
 func loadDataFromMainContent(uri *url.URL, z *html.Tokenizer) *propertyData {
+	var err error
+	check := func(name string) {
+		fmt.Println(name, uri.String()[50:], err)
+	}
 	data := new(propertyData)
-	data.uri = uri.String()
+	data.Uri = uri.String()
 	re := regexp.MustCompile(`\$(\d+(?:\.\d{2})?)`) // Matches $(123(.45)?)
-	fmt.Println("Loading ", uri)
 	// Find <h1 id="ListingTitle_title">
 	text, err := readTextFromTagWithAttr(z, "h1", "id", "ListingTitle_title")
 	if err != nil {
-		fmt.Println("Title", err)
+		check("Title")
 		return nil
 	}
-	data.title = text
+	data.Title = text
 
 	text, err = readTextFromTagWithAttr(z, "li", "id", "ListingTitle_classifiedTitlePrice")
 	if err != nil {
-		fmt.Println("Price", err)
+		check("Price")
 		return nil
 	}
 	matches := re.FindStringSubmatch(text)
 	if len(matches) != 2 {
-		fmt.Println("Regex", matches, text)
+		check("Regex Failed:" + strings.Join(matches, ",") + ":" + text)
 		return nil
 	}
 	priceF, err := strconv.ParseFloat(matches[1], 64)
 	if err != nil {
-		fmt.Println("Float Parse", err, matches)
+		check("Float Parse:" + strings.Join(matches, ","))
 		return nil
 	}
-	data.price = priceF
+	data.Price = priceF
 
-	findTagWithAttr(z, "ul", "id", "thumbs")
+	findTagWithAttr(z, "div", "id", "ListingPhotoAndAd")
 	err = readThumbnails(uri, z, data)
 	if err != nil {
-		fmt.Println("Thumbnails", err)
+		check("Thumbnails")
 		return nil
 	}
 
 	findTagWithAttr(z, "table", "id", "ListingAttributes")
 	err = readListAttrsTable(uri, z, data)
 	if err != nil {
-		fmt.Println("Attrs", err)
+		check("Attrs")
 		return nil
 	}
 
-	data.description, err = readTextFromTagWithAttr(z, "div", "id", "ListingDescription_ListingDescription")
+	data.Description, err = readTextFromTagWithAttr(z, "div", "id", "ListingDescription_ListingDescription")
+	if err != nil {
+		check("Description")
+		return nil
+	}
 
 	findTagWithAttr(z, "script", "id", "info-tooltip-tmpl")
 	mapScriptContent, err := readTextFromTagWithAttr(z, "script", "type", "text/javascript")
+	if err != nil {
+		check("Script")
+		return nil
+	}
 
-	fmt.Println(mapScriptContent)
-
-	fmt.Println(data)
+	err = parseMapScript(mapScriptContent, data)
+	if err != nil {
+		check("ParseScript")
+		return nil
+	}
 	return data
 }
 func readThumbnails(baseURL *url.URL, z *html.Tokenizer, data *propertyData) error {
@@ -327,19 +367,22 @@ func readThumbnails(baseURL *url.URL, z *html.Tokenizer, data *propertyData) err
 		tt := z.Next()
 		if tt == html.ErrorToken {
 			// Probably the end of the file ...
-			fmt.Println("Error: ", z.Err())
+			fmt.Println("RTError: ", z.Err())
 			return z.Err()
-		} else if tt == html.EndTagToken || tt == html.StartTagToken || tt == html.SelfClosingTagToken {
+		} else if tt == html.StartTagToken || tt == html.SelfClosingTagToken {
 			tagName := getTagName(z)
-			fmt.Println(tt, " -- ", tagName)
 			switch tagName {
-			case "ul":
-				size := len(thumbnails)
-				data.images = make([]string, size)
-				for imgUrl, _ := range thumbnails {
-					data.images = append(data.images, imgUrl)
+			case "div":
+				attrMap := getAttrs(z)
+				id, ok := attrMap["id"]
+				if ok && id == "advertSection" {
+					// Past the listing photos
+					data.Images = make([]string, 0)
+					for imgUrl, _ := range thumbnails {
+						data.Images = append(data.Images, imgUrl)
+					}
+					return nil
 				}
-				return nil
 			case "img":
 				attrMap := getAttrs(z)
 				src, ok := attrMap["src"]
@@ -382,10 +425,40 @@ func readListAttrsTable(baseURL *url.URL, z *html.Tokenizer, data *propertyData)
 		case html.EndTagToken:
 			tagName := getTagName(z)
 			if tagName == "table" {
-				data.otherData = attrsMap
+				data.OtherData = attrsMap
 				return nil
 			}
 		default:
 		}
 	}
+}
+
+func parseMapScript(mapScript string, data *propertyData) error {
+	getMatch := func(regex string, search string) string {
+		x := regexp.MustCompile(regex).FindStringSubmatch(search)
+		if x == nil {
+			return ""
+		} else {
+			return x[1]
+		}
+	}
+
+	lstId := getMatch(`listingId\s*:\s*(\d+)`, mapScript)
+	lat, err1 := strconv.ParseFloat(getMatch(`lat\s*:\s*(-?\d+\.\d+)`, mapScript), 64)
+	lng, err2 := strconv.ParseFloat(getMatch(`lng\s*:\s*(-?\d+\.\d+)`, mapScript), 64)
+	street := getMatch(`userEnteredLocation\s*:\s*"(.*)"\s?,`, mapScript)
+	suburb := getMatch(`structuredLocation\s*:\s*"(.*)"\s?,`, mapScript)
+
+	if err1 != nil {
+		return err1
+	} else if err2 != nil {
+		return err2
+	}
+
+	locationData := propertyLocationData{Lat: lat, Lng: lng, Street: street, Suburb: suburb}
+
+	data.ListingId = lstId
+	data.LocationData = locationData
+
+	return nil
 }
